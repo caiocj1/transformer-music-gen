d8ce96691d222280b176cb4034a1a97cd50f3144
Caio Jordan <caioj2906@gmail.com>
2023-02-03 13:18:52 +0100

    16 workers test

--- run_training.py
+++ run_training.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     # Initialize data module
     data_module = MusicDataModule(
         batch_size=8,
-        num_workers=20,
+        num_workers=16,
         max_samples=20
     )
     data_module.setup(stage='fit')
